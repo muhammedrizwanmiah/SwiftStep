3171031fbec86ca9dab9e00edf768713977a7a6b
Muhammed Rizwan Miah <r.miah@se18.qmul.ac.uk>
2021-02-05 20:55:54 +0000

    Login and Register functionality working on device switching between register and login page works (dont have account etc)

--- app/src/main/java/com/fitnessproject/LoginActivity.kt
+++ app/src/main/java/com/fitnessproject/LoginActivity.kt
@@ -1,12 +1,86 @@
 package com.fitnessproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        tv_register.setOnClickListener {
+            //moves from login activity to register activity
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+            finish()
+        }
+
+        btn_login.setOnClickListener {
+            when {
+                TextUtils.isEmpty(et_login_email.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(et_login_password.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+
+                    val email: String = et_login_email.text.toString().trim { it <= ' ' }
+                    val password: String = et_login_password.text.toString().trim { it <= ' ' }
+
+                    //Create an instance and create register user with email and pass
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+
+                                if (task.isSuccessful) {
+
+                                    Toast.makeText(
+                                        this@LoginActivity,
+                                        "You were signed in successfully.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    /** New user is registered and sent to main activity
+                                     **/
+
+                                    val intent =
+                                        Intent(this@LoginActivity, MainActivity::class.java)
+                                    intent.flags =
+                                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
+                                    intent.putExtra("email_id", email)
+                                    startActivity(intent)
+                                    finish() //closes register activity and means we are in main activity
+                                } else {
+                                    //if registration not successful show error message
+                                    Toast.makeText(
+                                        this@LoginActivity,
+                                        task.exception!!.message.toString(),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/fitnessproject/MainActivity.kt
+++ app/src/main/java/com/fitnessproject/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.fitnessproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userId = intent.getStringExtra("user_id")
+        val emailId = intent.getStringExtra("email_id")
+
+        tv_user_id.text = "User ID = $userId"
+        tv_email.text = "Email = $emailId"
+
+        btn_logout.setOnClickListener {
+            // Logs out from app
+            FirebaseAuth.getInstance().signOut()
+            //moves from main activity to login activity
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+            finish()
+        }
     }
 }
