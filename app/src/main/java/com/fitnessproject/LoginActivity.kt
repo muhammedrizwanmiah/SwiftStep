01e4d8a39e9244f3c87ba8f5ef30bc831f48bc9c
Muhammed Rizwan Miah <r.miah@se18.qmul.ac.uk>
2021-02-07 00:31:57 +0000

    Login and Register working with Firebase auth and realtime DB

--- app/src/main/java/com/fitnessproject/LoginActivity.kt
+++ app/src/main/java/com/fitnessproject/LoginActivity.kt
@@ -5,82 +5,58 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
-import com.google.firebase.auth.AuthResult
+import com.fitnessproject.ProfileActivity
+import com.fitnessproject.R
+import com.fitnessproject.RegistrationActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_register.*
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance()
 
-        tv_register.setOnClickListener {
-            //moves from login activity to register activity
-            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        val currentuser = auth.currentUser
+        if(currentuser != null) {
+            startActivity(Intent(this@LoginActivity, ProfileActivity::class.java))
             finish()
         }
+        login()
+    }
 
-        btn_login.setOnClickListener {
-            when {
-                TextUtils.isEmpty(et_login_email.text.toString().trim { it <= ' ' }) -> {
-                    Toast.makeText(
-                        this@LoginActivity,
-                        "Please enter email.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+    private fun login() {
 
-                TextUtils.isEmpty(et_login_password.text.toString().trim { it <= ' ' }) -> {
-                    Toast.makeText(
-                        this@LoginActivity,
-                        "Please enter password.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-                else -> {
+        loginButton.setOnClickListener {
 
-                    val email: String = et_login_email.text.toString().trim { it <= ' ' }
-                    val password: String = et_login_password.text.toString().trim { it <= ' ' }
-
-                    //Create an instance and create register user with email and pass
-                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener { task ->
-
-                                if (task.isSuccessful) {
-
-                                    Toast.makeText(
-                                        this@LoginActivity,
-                                        "You were signed in successfully.",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-
-                                    /** New user is registered and sent to main activity
-                                     **/
-
-                                    val intent =
-                                        Intent(this@LoginActivity, MainActivity::class.java)
-                                    intent.flags =
-                                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                    intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
-                                    intent.putExtra("email_id", email)
-                                    startActivity(intent)
-                                    finish() //closes register activity and means we are in main activity
-                                } else {
-                                    //if registration not successful show error message
-                                    Toast.makeText(
-                                        this@LoginActivity,
-                                        task.exception!!.message.toString(),
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
-                            }
-                }
+            if(TextUtils.isEmpty(usernameInput.text.toString())){
+                usernameInput.setError("Please enter username")
+                return@setOnClickListener
             }
+            else if(TextUtils.isEmpty(passwordInput.text.toString())){
+                usernameInput.setError("Please enter password")
+                return@setOnClickListener
+            }
+            auth.signInWithEmailAndPassword(usernameInput.text.toString(), passwordInput.text.toString())
+                .addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        startActivity(Intent(this@LoginActivity, ProfileActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this@LoginActivity, "Login failed, please try again! ", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+        }
+
+        registerText.setOnClickListener{
+            startActivity(Intent(this@LoginActivity, RegistrationActivity::class.java))
+
         }
     }
 }
