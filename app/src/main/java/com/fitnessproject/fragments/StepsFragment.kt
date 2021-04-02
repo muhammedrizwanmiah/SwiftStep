d5aa23acddb82f0cac6dda78ccd26349e3fc2415
Muhammed Rizwan Miah <r.miah@se18.qmul.ac.uk>
2021-04-02 20:12:11 +0100

    fixed stats bugs

--- app/src/main/java/com/fitnessproject/fragments/StepsFragment.kt
+++ app/src/main/java/com/fitnessproject/fragments/StepsFragment.kt
@@ -231,12 +231,13 @@ class StepsFragment : Fragment(), SensorEventListener {
 
                 tvTime.text = "Day: " + "$currentDay"
 
-                //calc stride length
+                //calc distance
                 val heightInCM = snapshot.child("height").value.toString().toDouble()
                 val strideLength = heightInCM * 0.43
                 val stepsInCM = (strideLength * tvStepsTaken.text.toString().toDouble()) / 100000
                 val distance = Math.round(stepsInCM * 100.0) / 100.0
 
+                //calc calories
                 val weightInKG = snapshot.child("weight").value.toString().toDouble()
                 val caloriesBurnedPerMile = 0.57 * (weightInKG * 2.2);
                 val stepCountMile = 160934.4 / strideLength;
@@ -244,9 +245,232 @@ class StepsFragment : Fragment(), SensorEventListener {
                 val caloriesBurned = tvStepsTaken.text.toString().toDouble() * conversionFactor;
                 val caloriesBurnedRounded = Math.round(caloriesBurned * 100.0) / 100.0
 
+                //**************STATS***********
+                // calculate stats
+                // most recent steps
+
+                /*
+                tvMostRecentMon.text = snapshot.child("user_data/most_recent_per_day/Mon").value.toString()
+                tvMostRecentTue.text = snapshot.child("user_data/most_recent_per_day/Tue").value.toString()
+                tvMostRecentWed.text = snapshot.child("user_data/most_recent_per_day/Wed").value.toString()
+                tvMostRecentThu.text = snapshot.child("user_data/most_recent_per_day/Thu").value.toString()
+                tvMostRecentFri.text = snapshot.child("user_data/most_recent_per_day/Fri").value.toString()
+                tvMostRecentSat.text = snapshot.child("user_data/most_recent_per_day/Sat").value.toString()
+                tvMostRecentSun.text = snapshot.child("user_data/most_recent_per_day/Sun").value.toString()
+                 */
+
+                //steps by day
+
+                //initalise var for steps by day
+                var stepsByMon = 0
+                var stepsByTue = 0
+                var stepsByWed = 0
+                var stepsByThu = 0
+                var stepsByFri = 0
+                var stepsBySat = 0
+                var stepsBySun = 0
+
+                //init cal
+                var calByMon = 0.0
+                var calByTue = 0.0
+                var calByWed = 0.0
+                var calByThu = 0.0
+                var calByFri = 0.0
+                var calBySat = 0.0
+                var calBySun = 0.0
+
+                //init distance
+                var distByMon = 0.0
+                var distByTue = 0.0
+                var distByWed = 0.0
+                var distByThu = 0.0
+                var distByFri = 0.0
+                var distBySat = 0.0
+                var distBySun = 0.0
+
+                //steps
+
+                stepsByMon = if (snapshot.child("user_data/counter_for_day/Mon").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_steps_by_day/Mon").value.toString()
+                        .toInt() / snapshot.child("user_data/counter_for_day/Mon").value.toString()
+                        .toInt()
+                } else {
+                    0
+                }
+
+                stepsByTue = if (snapshot.child("user_data/counter_for_day/Tue").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_steps_by_day/Tue").value.toString()
+                        .toInt() / snapshot.child("user_data/counter_for_day/Tue").value.toString()
+                        .toInt()
+                } else {
+                    0
+                }
+
+                stepsByWed = if (snapshot.child("user_data/counter_for_day/Wed").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_steps_by_day/Wed").value.toString()
+                        .toInt() / snapshot.child("user_data/counter_for_day/Wed").value.toString()
+                        .toInt()
+                } else {
+                    0
+                }
+
+                stepsByThu = if (snapshot.child("user_data/counter_for_day/Thu").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_steps_by_day/Thu").value.toString()
+                        .toInt() / snapshot.child("user_data/counter_for_day/Thu").value.toString()
+                        .toInt()
+                } else {
+                    0
+                }
+
+                stepsByFri = if (snapshot.child("user_data/counter_for_day/Fri").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_steps_by_day/Fri").value.toString()
+                        .toInt() / snapshot.child("user_data/counter_for_day/Fri").value.toString()
+                        .toInt()
+                } else {
+                    0
+                }
+
+                stepsBySat = if (snapshot.child("user_data/counter_for_day/Sat").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_steps_by_day/Sat").value.toString()
+                        .toInt() / snapshot.child("user_data/counter_for_day/Sat").value.toString()
+                        .toInt()
+                } else {
+                    0
+                }
+
+                stepsBySun = if (snapshot.child("user_data/counter_for_day/Sun").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_steps_by_day/Sun").value.toString()
+                        .toInt() / snapshot.child("user_data/counter_for_day/Sun").value.toString()
+                        .toInt()
+                } else {
+                    0
+                }
+
+
+
+                //calories by day
+
+                calByMon = if (snapshot.child("user_data/counter_for_day/Mon").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_calories_by_day/Mon").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Mon").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                calByTue = if (snapshot.child("user_data/counter_for_day/Tue").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_calories_by_day/Tue").value.toString().toDouble() / snapshot.child("user_data/counter_for_day/Tue").value.toString().toInt()
+                } else {
+                    0.0
+                }
+
+                calByWed = if (snapshot.child("user_data/counter_for_day/Wed").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_calories_by_day/Wed").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Wed").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                calByThu = if (snapshot.child("user_data/counter_for_day/Thu").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_calories_by_day/Thu").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Thu").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                calByFri = if (snapshot.child("user_data/counter_for_day/Fri").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_calories_by_day/Fri").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Fri").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                calBySat = if (snapshot.child("user_data/counter_for_day/Sat").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_calories_by_day/Sat").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Sat").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                calBySun = if (snapshot.child("user_data/counter_for_day/Sun").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_calories_by_day/Sun").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Sun").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+                //distance by day
+                distByMon = if (snapshot.child("user_data/counter_for_day/Mon").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_distance_by_day/Mon").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Mon").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                distByTue = if (snapshot.child("user_data/counter_for_day/Tue").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_distance_by_day/Tue").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Tue").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                distByWed = if (snapshot.child("user_data/counter_for_day/Wed").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_distance_by_day/Wed").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Wed").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                distByThu = if (snapshot.child("user_data/counter_for_day/Thu").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_distance_by_day/Thu").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Thu").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                distByFri = if (snapshot.child("user_data/counter_for_day/Fri").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_distance_by_day/Fri").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Fri").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                distBySat = if (snapshot.child("user_data/counter_for_day/Sat").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_distance_by_day/Sat").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Sat").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                distBySun = if (snapshot.child("user_data/counter_for_day/Sun").value.toString().toInt() !=0) {
+                    snapshot.child("user_data/total_distance_by_day/Sun").value.toString()
+                        .toDouble() / snapshot.child("user_data/counter_for_day/Sun").value.toString()
+                        .toInt()
+                } else {
+                    0.0
+                }
+
+                //all time stats
+                //all time steps, cal, dist
+                var allTimeSteps = snapshot.child("user_data/all_time/steps").value.toString()
+                var allTimeCal = snapshot.child("user_data/all_time/calories").value.toString()
+                var allTimeDist = snapshot.child("user_data/all_time/distance").value.toString()
+
+                //**************************************************
+
                 //text for elements on the screen
-                tvDistance.text = "Distance: " + distance.toString() + " km"
-                tvCalories.text = "Calories burnt: " + caloriesBurnedRounded.toString() + " cal"
+                tvDistance.text = distance.toString() + " km"
+                tvCalories.text = caloriesBurnedRounded.toString() + " kcal"
                 tvStepGoal.text = "/" + snapshot.child("stepgoal").value.toString()
 
                 yourHeightText.text = snapshot.child("height").value.toString() + " CM"
@@ -338,8 +562,8 @@ class StepsFragment : Fragment(), SensorEventListener {
                 val caloriesBurned = tvStepsTaken.text.toString().toDouble() * conversionFactor;
                 val caloriesBurnedRounded = Math.round(caloriesBurned * 100.0) / 100.0
 
-                tvCalories.text = "Calories burnt: " + caloriesBurnedRounded.toString() + " cal"
-                tvDistance.text = "Distance: " + distance.toString() + " km"
+                tvCalories.text = caloriesBurnedRounded.toString() + " kcal"
+                tvDistance.text = distance.toString() + " km"
 
                 currentCalories = caloriesBurnedRounded
                 currentDistance = distance
